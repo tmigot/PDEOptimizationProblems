127188cb00fbf3833870127cb2d942f3df0fa830
tmigot <tangi.migot@gmail.com>
2021-07-29 11:53:19 -0400

    add meta framework

--- src/PDEOptimizationProblems.jl
+++ src/PDEOptimizationProblems.jl
@@ -6,32 +6,17 @@
 #https://github.com/gridap/Gridap.jl
 #Cite: Badia, S., & Verdugo, F. (2020). Gridap: An extensible Finite Element toolbox in Julia. Journal of Open Source Software, 5(52), 2520.
 #
-# TODO/Improvements: - generate the inclusions of the files;
-#                    - make this folder an independant module.
-#                    - the parameter n should reflect the size of the problem
-#                    - keep making test problems out of the Gridap tutorials:
+# TODO/Improvements: - keep making test problems out of the Gridap tutorials:
 #                    remains 3, 4, 5, 6, 7, 9, 10, 11
 #                    https://gridap.github.io/Tutorials/stable/pages/t003_elasticity/
 #                    - take the models from Gridap.jl instead of copy-paste.
 #https://github.com/gridap/Gridap.jl/blob/master/test/GridapTests/PoissonTests.jl
 #https://github.com/gridap/Gridap.jl/blob/master/test/GridapTests/StokesTaylorHoodTests.jl
-#
-#Maybe this is better, but I prefer having comments on each link (for now)
-#=
-path = dirname(@__FILE__)
-files = filter(x -> x[end-2:end] == ".jl", readdir(path))
-for file in files
-  if file ≠ "OptimizationProblems.jl"
-    include(file)
-  end
-end
-=#
-#end
 ###############################################################################
 module PDEOptimizationProblems
 
+using DataFrames
 using Gridap
-
 using PDENLPModels
 
 const problems = [
@@ -82,6 +67,8 @@ const problems = [
   "membrane", # not the correct boundary condition
 ]
 
+const number_of_problems = length(problems)
+
 path = dirname(@__FILE__)
 files = filter(x -> x[end-2:end] == ".jl", readdir(path))
 for file in files
@@ -89,5 +76,68 @@ for file in files
     include(file)
   end
 end
+
+const pbtypes = [:θ, :y, :yu, :θy, :θyu]
+const origins = [:academic, :modelling, :real, :unknown]
+const objtypes = [:none, :constant, :linear, :quadratic, :sum_of_squares, :other]
+const contypes = [:unconstrained, :linear, :quadratic, :general]
+const names = [
+  :domaindim
+  :pbtype
+  :nθ
+  :ny
+  :nu
+  :optimalvalue
+  :objtype
+  :contype
+  :origin
+  :has_cvx_obj
+  :has_cvx_con
+  :has_bounds
+  :has_fixed_variables
+]
+
+const types = [
+  UInt8
+  Symbol
+  Int
+  Int # number of functions
+  Int # number of functions
+  Real
+  Symbol
+  Symbol
+  Symbol
+  Bool
+  Bool
+  Bool
+  Bool
+]
+
+"""
+    PDEOptimizationProblems.meta
+A composite type that represents the main features of the PDE-constrained optimization problem.
+    optimize    ∫( f(θ, y, u) )dΩ
+    subject to  lvar ≤ (θ, y, u)    ≤ uvar
+                ∫( res(θ, y, u, v) )dΩ = 0
+---
+The following keys are valid:
+Problem meta
+- `domaindim`: dimension of the domain 1/2/3 for 1D/2D/3D
+- `pbtype`: in pbtypes
+- `nθ`: size of the unknown vector
+- `ny`: number of unknown function
+- `nu`: number of control function
+Solution meta
+- `optimalvalue`: best known objective value (NaN if unknown, -Inf if unbounded problem)
+Classification
+- `objtype`: in objtypes
+- `contype`: in contypes
+- `origin`: in origins
+- `has_cvx_obj`: true if the problem has a convex objective
+- `has_cvx_con`: true if the problem has convex constraints
+- `has_bounds`: true if the problem has bound constraints
+- `has_fixed_variables`: true if it has fixed variables
+"""
+const meta = DataFrame(names .=> [Array{T}(undef, number_of_problems) for T in types])
 
 end #end of module
