53b8914e0b346f41973fdb36b28a144353a7fce8
tmigot <tangi.migot@gmail.com>
2021-10-07 23:15:20 +0200

    fix `rocket` problem

--- src/rocket.jl
+++ src/rocket.jl
@@ -2,6 +2,7 @@
 # https://www.mcs.anl.gov/~more//cops/cops3.pdf
 # n=400, 800, 1600
 function rocket(args...; n = 400, kwargs...)
+
   model = CartesianDiscreteModel((0, 1), n)
   labels = get_face_labeling(model)
   add_tag_from_tags!(labels, "diri0", [1]) #initial time condition
@@ -27,19 +28,25 @@ function rocket(args...; n = 400, kwargs...)
     labels = labels,
     dirichlet_tags = ["diri0", "diri1"],
   )
+  Vfree = TestFESpace(model, reffe; conformity = :H1)
   Yh = TrialFESpace(V0, h₀)
+  YH = TrialFESpace(Vfree)
   Yv = TrialFESpace(V0, 0.0)
   Ym = TrialFESpace(V1, [m₀, mᵪ * m₀])
   VS = TestFESpace(model, reffe; conformity = :L2)
   U = TrialFESpace(VS)
-  Xpde = MultiFieldFESpace([V0, V0, V1])
-  Ypde = MultiFieldFESpace([Yh, Yv, Ym])
+  Xpde = MultiFieldFESpace([V0, Vfree, V0, V1])
+  Ypde = MultiFieldFESpace([Yh, YH, Yv, Ym])
   Xcon = VS # MultiFieldFESpace([VS])
   Ycon = U # MultiFieldFESpace([U])
 
   trian = Triangulation(model)
   degree = 1
   dΩ = Measure(trian, degree)
+
+  degree = 1
+  Γ = BoundaryTriangulation(model, tags=["diri1"])
+  dΓ = Measure(Γ, degree)
 
   # for the weak formulation of dy/dt
   conv(u, ∇u) = (∇u ⋅ one(∇u)) ⊙ u
@@ -48,19 +55,24 @@ function rocket(args...; n = 400, kwargs...)
   D(h, v) = Dᵪ * v * v * (exp ∘ (-hᵪ * (h - h₀) / h₀))
   g(h) = g₀ * (h₀ / h) * (h₀ / h)
   function res(y, u, w)
-    h, v, m = y
-    ph, pv, pm = w
-    return ∫((c(h, ph) - v) + (c(v, pv) * m - (u - D(h, v)) + g(h) * m) + (c(m, pm) + u / cc))dΩ
+    h, H, v, m = y
+    ph, pH, pv, pm = w
+    return ∫(
+      (c(h, ph) - v * ph) +
+      (c(v, pv) * m - (u - D(h, v) - g(h) * m) * pv) +
+      (c(m, pm) + pm * u / cc) +
+      c(H, pH)
+    )dΩ + ∫( (H - h) * pH )dΓ
   end
   op = FEOperator(res, Ypde, Xpde)
 
   function f(y, u)
-    h, v, m = y
-    return ∫(-h)dΩ # we should maximize the altitude at final time
+    h, H, v, m = y
+    return ∫( -H )dΩ
   end
 
   ndofs_con = Gridap.FESpaces.num_free_dofs(Ycon)
-  # x0 should be h=1, v(t)=t*(1-t), m(t)=(mf-m0)*t+m0, T=Tmax/2
+  # x0 should be h=1, H=h v(t)=t*(1-t), m(t)=(mf-m0)*t+m0, T=Tmax/2
   cell_xs = get_cell_coordinates(trian)
   midpoint(xs) = sum(xs) / length(xs)
   cell_xm = lazy_map(midpoint, cell_xs)
@@ -68,6 +80,7 @@ function rocket(args...; n = 400, kwargs...)
   cell_m = lazy_map(x -> x * (mᵪ * m₀ - m₀) + m₀, cell_xm)
   xin = vcat(
     ones(Gridap.FESpaces.num_free_dofs(Yh)),
+    ones(Gridap.FESpaces.num_free_dofs(YH)),
     get_free_values(Gridap.FESpaces.interpolate(cell_v, Yv)),
     get_free_values(Gridap.FESpaces.interpolate(cell_m, Ym)),
     Tmax / 2 * ones(ndofs_con),
@@ -85,11 +98,13 @@ function rocket(args...; n = 400, kwargs...)
     uvaru = Tmax * ones(ndofs_con),
     lvary = vcat(
       h₀ * ones(Gridap.FESpaces.num_free_dofs(Yh)),
+      -Inf * ones(Gridap.FESpaces.num_free_dofs(YH)),
       zeros(Gridap.FESpaces.num_free_dofs(Yv)),
       mᵪ * m₀ * ones(Gridap.FESpaces.num_free_dofs(Ym)),
     ),
     uvary = vcat(
       Inf * ones(Gridap.FESpaces.num_free_dofs(Yh)),
+      Inf * ones(Gridap.FESpaces.num_free_dofs(YH)),
       Inf * ones(Gridap.FESpaces.num_free_dofs(Yv)),
       m₀ * ones(Gridap.FESpaces.num_free_dofs(Ym)),
     ),
@@ -102,15 +117,15 @@ rocket_meta = Dict(
   :domaindim => UInt8(1),
   :pbtype => :yu,
   :nθ => 0,
-  :ny => 3,
+  :ny => 4,
   :nu => 1,
   :optimal_value => NaN,
   :is_infeasible => false,
-  :objtype => :sum_of_squares,
+  :objtype => :linear,
   :contype => :general,
   :origin => :unknown,
   :deriv => typemax(UInt8),
-  :has_cvx_obj => false,
+  :has_cvx_obj => true,
   :has_cvx_con => false,
   :has_equalities_only => true,
   :has_inequalities_only => false,
@@ -118,4 +133,4 @@ rocket_meta = Dict(
   :has_fixed_variables => true,
 )
 
-get_rocket_meta(n::Integer = default_nvar) = (5 * n - 1, 3 * n - 1)
+get_rocket_meta(n::Integer = default_nvar) = (6 * n, 4 * n)
