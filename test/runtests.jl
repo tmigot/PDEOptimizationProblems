7da0bc9cd83f53fc45242e5a2669b147216718d3
tmigot <tangi.migot@gmail.com>
2021-10-14 12:13:04 -0400

    add timer in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,13 +6,15 @@ include("utils.jl")
 
 # Test that every problem can be instantiated.
 for prob in setdiff(names(PDEOptimizationProblems), [:PDEOptimizationProblems])
-  print(prob)
-  prob_fn = PDEOptimizationProblems.eval(prob)
-  nlp = prob_fn(n = 10)
-  println(" nvar=", nlp.meta.nvar, " ncon=", nlp.meta.ncon)
-  obj(nlp, nlp.meta.x0)
-  nlp.meta.ncon != 0 && cons(nlp, nlp.meta.x0)
-  hess(nlp, nlp.meta.x0, nlp.meta.y0)
-  # test meta information
-  meta_sanity(prob)
+  @time begin
+    print(prob)
+    prob_fn = PDEOptimizationProblems.eval(prob)
+    nlp = prob_fn(n = 10)
+    println(" nvar=", nlp.meta.nvar, " ncon=", nlp.meta.ncon)
+    obj(nlp, nlp.meta.x0)
+    nlp.meta.ncon != 0 && cons(nlp, nlp.meta.x0)
+    hess(nlp, nlp.meta.x0, nlp.meta.y0)
+    # test meta information
+    meta_sanity(prob)
+  end
 end
